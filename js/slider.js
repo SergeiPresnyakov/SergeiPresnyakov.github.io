34ff0564b5eaab5c73947da98c31a4329b72b06e
Sergei Presnyakov <jugg010203@gmail.com>
2021-04-13 16:14:11 +0800

    slider with pagination

--- js/slider.js
+++ js/slider.js
@@ -2,6 +2,7 @@ const SLIDER_MARGINS = ['0', '-33.3333%', '-66.6666%'];
 const btnNext = document.querySelector("#slideNext");
 const btnPrev = document.querySelector("#slidePrev");
 const slider = document.querySelector('.current');
+const sliderRadios = document.querySelectorAll('.d_page_radio');
 
 let index = 0;
 
@@ -10,6 +11,10 @@ btnNext.addEventListener('click', () => {
     if (index === SLIDER_MARGINS.length) {
         index = 0;
     }
+    sliderRadios.forEach((radio) => {
+        radio.classList.remove('d_page_active');
+    });
+    sliderRadios[index].classList.add('d_page_active');
     slider.style.marginLeft = SLIDER_MARGINS[index];
 });
 
@@ -18,5 +23,29 @@ btnPrev.addEventListener('click', () => {
     if (index < 0) {
         index = 2;
     }
+    sliderRadios.forEach((radio) => {
+        radio.classList.remove('d_page_active');
+    });
+    sliderRadios[index].classList.add('d_page_active');
     slider.style.marginLeft = SLIDER_MARGINS[index];
 });
+
+sliderRadios.forEach((radio, i) => {
+    activatePagination(radio, i);
+});
+
+function activatePagination(radio, i) {
+    radio.addEventListener('click', () => {
+        
+        for (let j = 0; j < 3; j++) {
+            if (i !== j) {
+                sliderRadios[j].classList.remove('d_page_active')
+            }
+        }
+        radio.classList.add('d_page_active');
+
+        index = i;
+        slider.style.marginLeft = SLIDER_MARGINS[index];
+    });
+}
+
